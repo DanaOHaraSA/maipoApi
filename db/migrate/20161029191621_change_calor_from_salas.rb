f128af8ff32b1412529418ccd92da450aba0dfe3
DanaOHara <rosa.munozv@usach.cl>
2016-10-29 20:47:47 -0300

    valor???

--- db/migrate/20161029191621_change_calor_from_salas.rb
+++
@@ -1,5 +0,0 @@
-class ChangeCalorFromSalas < ActiveRecord::Migration[5.0]
-  def change
-    rename_column :salas, :calor, :valor
-  end
-end
--- db/migrate/20161029232819_change_valor_from_salas.rb
+++
@@ -1,5 +0,0 @@
-class ChangeValorFromSalas < ActiveRecord::Migration[5.0]
-  def change
-   change_column :salas, :valor, :float
-  end
-end
